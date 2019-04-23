61e17978c04ecb8156b4937b375e1856fb6bebe1
Ryan King <hello@ryanking.com>
2019-04-23 18:00:05 -0400

    feat: get basic read/write working

--- internal/store/store.go
+++ internal/store/store.go
@@ -1,6 +1,7 @@
 package store
 
 import (
+	"encoding/json"
 	"fmt"
 	"strings"
 	"time"
@@ -8,6 +9,7 @@ import (
 	"os"
 
 	"github.com/RyanTKing/idid/internal/config"
+	"github.com/RyanTKing/idid/internal/git"
 )
 
 // Write writes a message with related issues to the store
@@ -21,8 +23,48 @@ func Write(msg string, issues ...string) error {
 	return write(now, dir, msg, issues...)
 }
 
-func write(now time.Time, dir, msg string, issues ...string) error {
-	return nil
+func write(now time.Time, dir, msg string, issueShorthands ...string) error {
+	path := getPath(now, dir)
+	f, err := os.Open(path)
+	entries := []Entry{}
+	if err == nil {
+		err := json.NewDecoder(f).Decode(&entries)
+		if err != nil {
+			f.Close()
+			return err
+		}
+		err = os.Remove(path)
+		if err != nil {
+			f.Close()
+			return err
+		}
+		f.Close()
+	} else if !os.IsNotExist(err) {
+		return err
+	}
+
+	issues := []Issue{}
+	for _, shorthand := range issueShorthands {
+		url, err := git.ExpandLink(shorthand)
+		if err != nil {
+			return err
+		}
+		issue := Issue{
+			Shorthand: shorthand,
+			URL:       url,
+		}
+		issues = append(issues, issue)
+	}
+	entry := Entry{
+		Msg:     msg,
+		Issues:  issues,
+		Created: now,
+	}
+	entries = append(entries, entry)
+
+	f, err = os.OpenFile(path, os.O_RDWR|os.O_CREATE, os.ModePerm)
+	defer f.Close()
+	return json.NewEncoder(f).Encode(entries)
 }
 
 // Read reads the messages written in the last 24 hours and returns the entry
@@ -37,7 +79,27 @@ func Read() ([]Entry, error) {
 }
 
 func read(now time.Time, dir string) ([]Entry, error) {
-	return []Entry{}, nil
+	path := getPath(now, dir)
+	f, err := os.Open(path)
+	defer f.Close()
+	entries := []Entry{}
+	if os.IsNotExist(err) {
+		return []Entry{}, nil
+	} else if err != nil {
+		return []Entry{}, nil
+	}
+
+	err = json.NewDecoder(f).Decode(&entries)
+	if err != nil {
+		return []Entry{}, err
+	}
+
+	return entries, nil
+}
+
+func getPath(now time.Time, dir string) string {
+	y, m, d := now.Date()
+	return fmt.Sprintf("%s/%2d%02d%02d.json", dir, y, m, d)
 }
 
 func getDirectory() (string, error) {
