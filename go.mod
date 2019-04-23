6ba9b96880c3ee19b2f67150eb20735d541e6401
Ryan King <hello@ryanking.com>
2019-04-22 23:51:18 -0400

    test: write tests for git expand link

--- go.mod
+++ go.mod
@@ -3,9 +3,12 @@ module github.com/RyanTKing/idid
 go 1.12
 
 require (
-	github.com/RyanTKing/reddix v0.0.0-20190407021955-bbf60178f4f1
-	github.com/nsf/termbox-go v0.0.0-20190325093121-288510b9734e // indirect
+	github.com/BurntSushi/toml v0.3.1 // indirect
+	github.com/inconshreveable/mousetrap v1.0.0 // indirect
+	github.com/spf13/afero v1.2.1 // indirect
 	github.com/spf13/cobra v0.0.3
 	github.com/spf13/viper v1.3.2
 	github.com/stretchr/testify v1.2.2
+	golang.org/x/sys v0.0.0-20190222171317-cd391775e71e // indirect
+	gopkg.in/h2non/gock.v1 v1.0.14
 )
---
+++ internal/git/git_test.go
@@ -0,0 +1,62 @@
+package git
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+	"testing"
+
+	"github.com/RyanTKing/idid/internal/config"
+	"github.com/stretchr/testify/suite"
+	"gopkg.in/h2non/gock.v1"
+)
+
+type GitTestSuite struct {
+	suite.Suite
+}
+
+func (suite *GitTestSuite) TearDownTest() {
+	gock.RestoreClient(client)
+	gock.Off()
+}
+
+func (suite *GitTestSuite) TestExpandLink() {
+	assert := suite.Assert()
+	require := suite.Require()
+	tests := []struct {
+		shorthand string
+		link      string
+		err       error
+	}{
+		{"myorg/myrepo#22", "https://github.com/myorg/myrepo/issues/22", nil},
+		{"myorg/myrepo#48", "https://github.com/myorg/myrepo/pull/48", nil},
+		{"myorg/myrepo#51", "", ErrNotFound},
+		{"myorg/myrepo#1001", "", fmt.Errorf("received code 418 from GitHub")},
+		{"myorg/myrepo12", "", ErrMalformedShortHand},
+		{"myorg#myrepo#22", "", ErrMalformedShortHand},
+		{"myorg/myrepo#foo", "", &strconv.NumError{Func: "Atoi", Num: "foo", Err: strconv.ErrSyntax}},
+	}
+
+	cfg := config.Get()
+	gock.InterceptClient(client)
+	gock.New(cfg.GitHub.Endpoint).Get("/myorg/myrepo/issues/22").Reply(http.StatusOK)
+	gock.New(cfg.GitHub.Endpoint).Get("/myorg/myrepo/issues/48").
+		Reply(http.StatusFound).AddHeader("Location", "https://github.com/myorg/myrepo/pull/48")
+	gock.New(cfg.GitHub.Endpoint).Get("/myorg/myrepo/issues/51").Reply(http.StatusNotFound)
+	gock.New(cfg.GitHub.Endpoint).Get("/myorg/myrepo/issues/1001").Reply(http.StatusTeapot)
+
+	for _, tt := range tests {
+		link, err := ExpandLink(tt.shorthand)
+		if !assert.Equal(tt.err, err, "For: %s", tt.shorthand) {
+			continue
+		}
+		assert.Equal(tt.link, link, "For: %s", tt.shorthand)
+	}
+
+	require.True(gock.IsDone())
+}
+
+func TestGitTestSuite(t *testing.T) {
+	tests := new(GitTestSuite)
+	suite.Run(t, tests)
+}
